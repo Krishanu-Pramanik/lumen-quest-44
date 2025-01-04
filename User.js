66939066165a91519de7b21831921cf4f6aebff1
Krishanu-Pramanik <116421896+Krishanu-Pramanik@users.noreply.github.com>
2025-01-04 11:05:22 +0530

    Create User.js

---
+++ User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    enum: ['Admin', 'Manager', 'User'],
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Export the User model
+module.exports = mongoose.model('User', userSchema);
