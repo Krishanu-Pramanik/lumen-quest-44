1a8787abfb31f9d985dc8e20fabd2f1173e98db4
Krishanu-Pramanik <116421896+Krishanu-Pramanik@users.noreply.github.com>
2025-01-04 11:13:08 +0530

    Update User.js

--- User.js
+++ User.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['Admin', 'Manager', 'User'],
+    enum: ['Admin', 'Manager', 'Staff', 'User'],
     required: true,
   },
   email: {
